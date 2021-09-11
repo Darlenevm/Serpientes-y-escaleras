6b8e6b99fe723c8cae24396268ecced61d5e165a
Darlenevm <dmoreno9@ucol.mx>
2021-09-10 23:18:32 -0500

    terminado

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-import Positions from "./positions";
+import Positions from "./positions.js";
 
 class App{
     constructor(){
         this.board = new Array();
-        for(let i=0;i<110;i++){
+        for(let i=0;i<106;i++){
             this.board.push(i);
         }
-        //console.log(this.board);
         //Escaleras
-        this.board[3] = 10;
-        this.board[5] = 11;
+        this.board[2] = 18;
+        this.board[8] = 30;
         this.board[23] = 53;
-        this.board[30] =48;
-        this.board[32] = 78;
-        this.board[50] = 65;
-        this.board[80] = 88;
-        this.board[90] = 98;
+        this.board[37] = 60;
+        this.board[49] = 80;
+        this.board[52] = 63;
+        this.board[71] = 87;
+        this.board[82] = 96;
         //Serpientes
-        this.board[6] = 2;
-        this.board[12] = 10;
-        this.board[20] = 4;
-        this.board[35] = 21;
-        this.board[39] = 19;
-        this.board[55] = 48;
-        this.board[87] = 85;
-        this.board[99] = 1;
+        this.board[10] = 1;
+        this.board[15] = 3;
+        this.board[27] = 4;
+        this.board[39] = 4;
+        this.board[44] = 19;
+        this.board[58] = 20;
+        this.board[79] = 14;
+        this.board[99] = 3;
+        
         console.log(this.board);
         console.log(this.game());
 
@@ -40,10 +40,10 @@ class App{
         let j2 = new Positions();
         while(j1.currentPos()<100 && j2.currentPos()<100){
             j1.addPosition(this.dice());
-            j1.newPosition(this.tableroNuevo[j1.currentPos()]);
+            j1.newPosition(this.board[j1.currentPos()]);
             console.log(j1.currentPos());
             j2.addPosition(this.dice());
-            j2.newPosition(this.tableroNuevo[j2.currentPos()]);
+            j2.newPosition(this.board[j2.currentPos()]);
             console.log(j2.currentPos());
         }
 
@@ -51,8 +51,10 @@ class App{
             return "Player 1 WINS!"
         }
         else{
-            return "Player 1 WINS!"
+            return "Player 2 WINS!"
         }
+
+        
     }
 }
 new App;
--- positions.js
+++ positions.js
@@ -11,7 +11,7 @@ export default class Positions{
         this.position += position;
     }
 
-    newPosition(){
+    newPosition(position){
         this.position = position;
     }
 
